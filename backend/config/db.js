e900e0069b329a15e55cf90e3db9f32ea092313d
Jorge Meza Bello <jorberiver@gmail.com>
2023-07-15 01:48:37 -0600

    ocultamiento las variables de entorno

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,17 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect("mongodb+srv://root:rootroot@cluster0.egezjtb.mongodb.net/testNodeBank?retryWrites=true&w=majority");
+        const connection = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        const url = `${connection.connection.host}:${connection.connection.port}`;
+        console.log(`Mongo DB conectado en ${url}`);
     } catch (error) {
         console.log(`error: ${error.message }`);
         process.exit(1);
     }
 }
+
+export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
 
 const app = express();
+const port = process.env.PORT || 4000;
 
-app.listen(4000, () => {
-    console.log("Corriendo en el puerto 4000");
+dotenv.config();
+connectDB();
+
+
+app.listen(port, () => {
+    console.log(`Corriendo en el puerto ${port}`);
 })
