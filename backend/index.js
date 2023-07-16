ee20176b0ebe0b81d40b8437b08dfec085ae89b8
Jorge Meza Bello <jorberiver@gmail.com>
2023-07-16 09:03:30 -0600

    Router usuarios creado

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import usuarioRouter from "./route/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -8,6 +9,7 @@ const port = process.env.PORT || 4000;
 dotenv.config();
 connectDB();
 
+app.use("/api/users", usuarioRouter)
 
 app.listen(port, () => {
     console.log(`Corriendo en el puerto ${port}`);
