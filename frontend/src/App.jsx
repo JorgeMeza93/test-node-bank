d760e5049fdf2f3e8b70947659a774932224d8b8
Jorge Meza Bello <jorberiver@gmail.com>
2023-07-18 22:10:20 -0600

    formulario login react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./paginas/Login";
+import SignUp from "./paginas/Sign-Up";
 
 function App() {
 
   return (
-    <div className="App"> 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout/>}>
+            <Route index element={<Login/> }/>
+            <Route path="signup" element={<SignUp/>} />
+        </Route>
+        <Route>
 
-    </div>
-      
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
