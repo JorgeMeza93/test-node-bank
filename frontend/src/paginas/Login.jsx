f804db48af4e9c649c150055d1c21dc5c4f26c41
Jorge Meza Bello <jorberiver@gmail.com>
2023-07-19 15:45:51 -0600

    validación campos vacíos login

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Alerta from '../components/Alerta';
+import axios from 'axios';
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("pipo");
+    if([email, password].includes("")){
+      setAlerta({ msg: "All the fields are required!", error: true })
+      return;
+    }
+  }
+
+  const { msg } = alerta;
+
   return (
     <div>
       <img src='logo.png' className='w-1/5 block mx-auto'/>
       <h1 className='text-sky-500 mt-5 text-center font-black text-xl'>Log In to Online Banking</h1>
-      <form className='my-10 bg-white shadow rounded-lg px-10 py-10'>
+      { msg && <Alerta alerta={alerta} /> }
+      <form className='my-10 bg-white shadow rounded-lg px-10 py-10' onSubmit={handleSubmit}>
         <div className='my-5'>
           <label className='text-gray-600 block text-xl' htmlFor='email'>Email</label>
-          <input id='email' name='email' type='email' className='w-full mt-3 p-3 border rounded-xl bg-gray-50'></input>
+          <input id='email' name='email' type='email' className='w-full mt-3 p-3 border rounded-xl bg-gray-50' value={email} onChange={ e => setEmail(e.target.value)}></input>
         </div>
         <div className='my-5'>
           <label className='text-gray-600 block text-xl' htmlFor='password'>Password</label>
-          <input id="password" name='password' type='password' className='w-full mt-3 p-3 border rounded-xl bg-gray-50'></input>
+          <input id="password" name='password' type='password' className='w-full mt-3 p-3 border rounded-xl bg-gray-50' value={password} onChange={ e => setPassword(e.target.value)}></input>
         </div>
         <input type='submit' value="Login" className='bg-gray-300 text-black w-2/3 block mx-auto font-bold text-xl rounded-xl py-3 mt-14 uppercase mb-5' />
       </form>
